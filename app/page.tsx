e5024215546104ef7a3732b5d8284718f1b3a0e0
Narrence Code <nadicode552@gmail.com>
2026-04-10 17:47:08 +0700

    change text home

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1 className="text-3xl font-bold mb-6 text-center">
-            QR Generator
+            QR Generator Simple
         </h1>
 
         <QRGenerator />
