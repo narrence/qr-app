42d091a85eabdc8e818eeeff6bf2e64f9f4c90d4
Narrence Code <nadicode552@gmail.com>
2026-04-13 13:35:47 +0700

    ui design

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,16 @@ import QRGenerator from "@/components/QRGenerator";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1 className="text-3xl font-bold mb-6 text-center">
-            QR Generator Simple
-        </h1>
+    <div className="flex flex-col flex-1 min-h-screen items-center justify-center font-sans bg-gradient-to-b from-[#DFE2FF] to-[#FCFCFD]">
+      <main className="flex flex-1 w-full max-w-[1248px] flex-col justify-center sm:items-start space-y-12">
+        <div>
+          <h1 className="text-6xl font-bold mb-3 text-[#5333CF]">
+              Easier Link to QR Code
+          </h1>
+          <p className="text-2xl font-medium text-[#5333CF]">
+              Helping you to make QR with easier step and simple
+          </p>
+        </div>
 
         <QRGenerator />
       </main>
--- components/QRGenerator.tsx
+++ components/QRGenerator.tsx
@@ -36,55 +36,60 @@ export default function QRGenerator() {
     };
 
     return (
-        <div className="space-y-6">
-            {/* INPUT */}
-            <input
-                className="border p-3 w-full rounded"
-                placeholder="https://example.com"
-                value={url}
-                onChange={(e) => setUrl(e.target.value)}    
-            />
-        
-            {/* TOGGLE */}
-            <div className="flex gap-4">
-                <button
-                onClick={() => setMode("static")}
-                className={`px-4 py-2 rounded ${
-                    mode === "static" ? "bg-black text-white" : "border"
-                }`}
-                > static
-                </button>
+        <div className="flex flex-row w-full max-width-[1248px] bg-gradient-to-b from-[#432CA7] to-[#3A2B84] p-[16px] rounded-[24px] shadow space-x-4">
+            
+            <div className="flex-col w-full space-y-4">
+                {/* INPUT */}
+                <input
+                    className="border-1 border-[#7060F6] p-3 w-full rounded-[12px]"
+                    placeholder="ex: https://example.com"
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}    
+                />
+            
+                {/* TOGGLE */}
+                <div className="flex gap-4">
+                    <button
+                    onClick={() => setMode("static")}
+                    className={`px-4 py-2 rounded min-w-[124px] ${
+                        mode === "static" ? "bg-[#432CA7] border-1 border-[#EDEFFF] font-bold text-white rounded-[12px]" : "bg-[#3A2B84] border-1 border-[#7060F6] rounded-[12px]"
+                    }`}
+                    > Static
+                    </button>
 
+                    <button
+                    onClick={() => setMode("dynamic")}
+                    className={`px-4 py-2 rounded min-w-[124px] ${
+                        mode === "dynamic" ? "bg-[#432CA7] border-1 border-[#EDEFFF] font-bold text-white rounded-[12px]" : "bg-[#3A2B84] border-1 border-[#7060F6] rounded-[12px]"
+                    }`}
+                    >
+                    Dynamic
+                    </button>
+                </div>
+
+                {/* GENERATE */}
                 <button
-                onClick={() => setMode("dynamic")}
-                className={`px-4 py-2 rounded ${
-                    mode === "dynamic" ? "bg-black text-white" : "border"
-                }`}
+                onClick={generate}
+                className="bg-[#7060F6] text-white px-6 py-3 rounded w-full"
                 >
-                Dynamic
+                    Do Magic Now
                 </button>
             </div>
+            
+            <div className="flex w-full max-w-[424px] bg-[#A2A6FF] rounded-[16px]">
+                {/* RESULT */}
+                {qr && (
+                    <div className="text-center space-y-3">
+                        <img src={qr} alt="QR Code" className="mx-auto" />
 
-            {/* GENERATE */}
-            <button
-            onClick={generate}
-            className="bg-black text-white px-6 py-3 rounded w-full"
-            >
-                Generate QR
-            </button>
-
-            {/* RESULT */}
-            {qr && (
-                <div className="text-center space-y-3">
-                    <img src={qr} alt="QR Code" className="mx-auto" />
-
-                    {mode === "dynamic" && (
-                        <p className="text-sm text-gray-500">
-                            {window.location.origin}/q/{slug}
-                        </p>
-                    )}
-                </div>
-            )}
+                        {mode === "dynamic" && (
+                            <p className="text-sm text-gray-500">
+                                {window.location.origin}/q/{slug}
+                            </p>
+                        )}
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
