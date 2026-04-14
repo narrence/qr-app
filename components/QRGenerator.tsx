7f1248256d1ec8a026981131ce0ac13fca8dec1c
Narrence Code <nadicode552@gmail.com>
2026-04-14 11:27:58 +0700

    fix ui design

--- components/QRGenerator.tsx
+++ components/QRGenerator.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import QRCode from "qrcode";
+import { WandSparkles  } from "lucide-react";
 
 export default function QRGenerator() {
     const [url, setUrl] = useState("");
@@ -37,43 +38,50 @@ export default function QRGenerator() {
 
     return (
         <div className="flex flex-row w-full max-width-[1248px] bg-gradient-to-b from-[#432CA7] to-[#3A2B84] p-[16px] rounded-[24px] shadow space-x-4">
-            
-            <div className="flex-col w-full space-y-4">
-                {/* INPUT */}
-                <input
-                    className="border-1 border-[#7060F6] p-3 w-full rounded-[12px]"
-                    placeholder="ex: https://example.com"
-                    value={url}
-                    onChange={(e) => setUrl(e.target.value)}    
-                />
-            
-                {/* TOGGLE */}
-                <div className="flex gap-4">
-                    <button
-                    onClick={() => setMode("static")}
-                    className={`px-4 py-2 rounded min-w-[124px] ${
-                        mode === "static" ? "bg-[#432CA7] border-1 border-[#EDEFFF] font-bold text-white rounded-[12px]" : "bg-[#3A2B84] border-1 border-[#7060F6] rounded-[12px]"
-                    }`}
-                    > Static
-                    </button>
+            <div className="flex w-full p-[16px]">
+                <div className="flex-col w-full space-y-4">
+                    {/* INPUT */}
+                    <input
+                        className="border-1 border-[#7060F6] p-3 w-full rounded-[12px]
+                                    hover:border-violet-200 
+                                    focus:border-violet-300 focus:ring-2 focus:ring-violet-200 
+                                    outline-none transition"
+                        placeholder="ex: https://example.com"
+                        value={url}
+                        onChange={(e) => setUrl(e.target.value)}    
+                    />
+                
+                    {/* TOGGLE */}
+                    <div className="flex gap-4">
+                        <button
+                        onClick={() => setMode("static")}
+                        className={`px-4 py-2 rounded min-w-[124px] ${
+                            mode === "static" ? "bg-[#432CA7] border-1 border-[#EDEFFF] font-bold text-white rounded-[12px]" : "bg-[#3A2B84] border-1 border-[#7060F6] rounded-[12px]"
+                        }`}
+                        > Static
+                        </button>
 
+                        <button
+                        onClick={() => setMode("dynamic")}
+                        className={`px-4 py-2 rounded min-w-[124px] ${
+                            mode === "dynamic" ? "bg-[#432CA7] border-1 border-[#EDEFFF] font-bold text-white rounded-[12px]" : "bg-[#3A2B84] border-1 border-[#7060F6] rounded-[12px]"
+                        }`}
+                        >
+                        Dynamic
+                        </button>
+                    </div>
+
+                    {/* GENERATE */}
                     <button
-                    onClick={() => setMode("dynamic")}
-                    className={`px-4 py-2 rounded min-w-[124px] ${
-                        mode === "dynamic" ? "bg-[#432CA7] border-1 border-[#EDEFFF] font-bold text-white rounded-[12px]" : "bg-[#3A2B84] border-1 border-[#7060F6] rounded-[12px]"
-                    }`}
+                    onClick={generate}
+                    className="flex flex-row gap-3 items-center justify-center bg-[#7060F6] text-white px-6 py-3 rounded-[12px] w-full
+                                hover:bg-[#817DFC] hover:scale-103 hover:rotate-1
+                                transition"
                     >
-                    Dynamic
+                        <WandSparkles size={24}/>
+                        Do Magic Now
                     </button>
                 </div>
-
-                {/* GENERATE */}
-                <button
-                onClick={generate}
-                className="bg-[#7060F6] text-white px-6 py-3 rounded w-full"
-                >
-                    Do Magic Now
-                </button>
             </div>
             
             <div className="flex w-full max-w-[424px] bg-[#A2A6FF] rounded-[16px]">
