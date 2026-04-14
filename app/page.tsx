951fc465d645331e56c12765bd8e06a261827ee1
Narrence Code <nadicode552@gmail.com>
2026-04-14 15:17:47 +0700

    .

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 min-h-screen items-center justify-center font-sans bg-gradient-to-b from-[#DFE2FF] to-[#FCFCFD]">
       <main className="flex flex-1 w-full max-w-[1248px] flex-col justify-center sm:items-start space-y-12">
-        <div>
+        <div className="flex flex-col w-full items-center justify-center">
           <h1 className="text-6xl font-bold mb-3 text-[#5333CF]">
               Easier Link to QR Code
           </h1>
