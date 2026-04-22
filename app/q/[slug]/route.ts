5e620d0bd59b00e33d0237cc09e79f87bbab38e8
Narrence Code <nadicode552@gmail.com>
2026-04-22 11:11:54 +0700

    setting route

--- app/q/[slug]/route.ts
+++ app/q/[slug]/route.ts
@@ -1,11 +1,11 @@
 import { supabase } from "@/lib/supabase";
 import { supabaseServer } from "@/lib/supabaseServer";
 import { redirect } from "next/navigation";
-import { NextRequest } from "next/server";
+// import { NextRequest } from "next/server";
 
 export async function GET(
-    req: NextRequest,
-    { params }: { params: Promise<{ slug: string }> }
+    req: Request,
+    { params }: { params: { slug: string } }
 ) {
     const { slug } = await params;
     const { data } = await supabaseServer
@@ -26,3 +26,27 @@ export async function GET(
     //redirect
     redirect(data.original_url);
 }
+
+// export async function GET(
+//     req: NextRequest,
+//     { params }: { params: Promise<{ slug: string }> }
+// ) {
+//     const { slug } = await params;
+//     const { data } = await supabaseServer
+//         .from("qr_codes")
+//         .select("*")
+//         .eq("slug", slug)
+//         .single();
+
+//     if (!data) {
+//         return new Response("Not Found", {status: 404});
+//     }
+//     //Update Scan Count
+//     await supabaseServer
+//         .from("qr_codes")
+//         .update({ scans: data.scans + 1 })
+//         .eq("slug", slug); 
+
+//     //redirect
+//     redirect(data.original_url);
+// }
